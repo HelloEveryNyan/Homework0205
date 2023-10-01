6a87eeee2c53a1387f3dd4bd80cc23c22ee3ec2b
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-10-01 17:56:07 +0200

    Add files via upload
    задача двумерный массив

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿int[,] matrix = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int[] rowArray = new int[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        rowArray[j] = matrix[i, j];
+    }
+    System.Array.Sort(rowArray, (a, b) => -a.CompareTo(b));
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rowArray[j];
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write(matrix[i, j] + " ");
+    }
+    System.Console.WriteLine();
+}
