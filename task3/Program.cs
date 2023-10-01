028ff981aeaea6f1fa83f8483bcb0447ad56e73a
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-10-01 17:58:09 +0200

    две матрицы
    две матрицы

---
+++ task3/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] matrix1 = {
+    {4, 4},
+    {3, 3}
+};
+
+int[,] matrix2 = {
+    {2, 2},
+    {5, 5}
+};
+
+int[,] result = MultiplyMatrices(matrix1, matrix2);
+
+PrintMatrix(result);
+
+static int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+{
+    int rows1 = matrix1.GetLength(0);
+    int cols1 = matrix1.GetLength(1);
+    int cols2 = matrix2.GetLength(1);
+
+    int[,] result = new int[rows1, cols2];
+
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < cols2; j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < cols1; k++)
+            {
+                sum += matrix1[i, k] * matrix2[k, j];
+            }
+            result[i, j] = sum;
+        }
+    }
+
+    return result;
+}
+
+static void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
