a982115cdeddc13e9cfa7fe106b6a02f10924a38
Евгений <142923731+HelloEveryNyan@users.noreply.github.com>
2023-10-01 17:59:13 +0200

    массив в 3D UltraHD
    трёхмерный массив

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,,] array3D = {
+    {
+        {66, 25},
+        {34, 41}
+    },
+    {
+        {27, 90},
+        {26, 55}
+    }
+};
+
+for (int x = 0; x < 2; x++)
+{
+    for (int y = 0; y < 2; y++)
+    {
+        for (int z = 0; z < 2; z++)
+        {
+            Console.WriteLine($"{array3D[x, y, z]}({x},{y},{z})");
+        }
+    }
+}
